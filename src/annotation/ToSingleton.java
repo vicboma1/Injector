094f48669dc2967e8b3d1a087f2718b710cd19f8
Victor Bolinches <vicboma1@users.noreply.github.com>
2014-09-28 23:51:18 +0200

    annotations

---
+++ src/annotation/ToSingleton.java
@@ -0,0 +1,17 @@
+package annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by vicboma on 28/09/14.
+ */
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface ToSingleton {
+    String fromInterface();
+}
+
